e25d155b6a17f6d31b309817252e92422ff9a8d6
arundhaas1 <arunram.9215@gmail.com>
2021-07-21 21:45:13 +0530

    add correction

--- src/Components/Events/Next.js
+++ src/Components/Events/Next.js
@@ -5,7 +5,7 @@ import Eventdata from './Eventdata'
 function Next() {
   return (
     <div className="next">
-      <h1>Our Next Projects</h1>
+      <h1>Our Next Events</h1>
       <p>
         Vestibulum eget lacus at mauris sagittis varius. Etiam ut venenatis dui.
         Nullam tellus risus, pellentesque at facilisis et, scelerisque sit amet
