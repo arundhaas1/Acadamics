a2efbc13e33568285f9e55d0919913273c373ddb
arundhaas1 <arunram.9215@gmail.com>
2021-07-23 18:24:56 +0530

    finish

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,19 +6,20 @@ import EmailIcon from "@material-ui/icons/Email";
 
 function Contact() {
   return (
+
     <div className="contact">
       <div className="contact__info">
         <h1>CONTACT INFO</h1>
         <div className="numbers">
           <div className="number">
-            <PhoneIcon className="ics"  fontSize="large" />
+            <PhoneIcon className="ics" fontSize="large" />
             <div className="cons__data">
               <h4>PHONE</h4>
               <p>+91-11-26382314</p>
             </div>
           </div>
           <div className="number">
-            <LocationOnIcon className="ics"  fontSize="large" />
+            <LocationOnIcon className="ics" fontSize="large" />
             <div className="cons__data">
               <h4>LOCATION</h4>
               <p>GCE 110020 - Salem</p>
