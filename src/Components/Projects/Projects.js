b2fc34a4e57a6c2f0d5bc090bee78b69399a38bc
arundhaas1 <arunram.9215@gmail.com>
2021-08-07 22:30:17 +0530

    add

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -16,13 +16,13 @@ function Projects() {
       </h5>
       <p>Few of them are shown below </p>
       <div className="projects__pro">
-        <Project image={"https://infallible-aryabhata-ff6849.netlify.app/assets/Whatsapp.3829d4dd52a8fc0128a56244d0c46b18.png"}/>
-        <Project image={"https://infallible-aryabhata-ff6849.netlify.app/assets/amazon2.32aa4762b3fcea0a1e142ebde1fd9e00.png"}/>
-        <Project image={"https://infallible-aryabhata-ff6849.netlify.app/assets/Netflix.3439282b685a69b08a955e79dd66c512.png"} />
+        <Project image={"https://arundhaas1.netlify.app/assets/Whatsapp.3829d4dd52a8fc0128a56244d0c46b18.png"}/>
+        <Project image={"https://arundhaas1.netlify.app/assets/amazon2.32aa4762b3fcea0a1e142ebde1fd9e00.png"}/>
+        <Project image={"https://arundhaas1.netlify.app/assets/Netflix.3439282b685a69b08a955e79dd66c512.png"} />
         <Project image={"https://vishal2612200.github.io/college-website-design/images/project/homepage.jpg"} />
-        <Project image={"https://infallible-aryabhata-ff6849.netlify.app/assets/Messenger.26f3f3fdbee968d8e4f546ad07befa47.png"} />
-        <Project image={"https://infallible-aryabhata-ff6849.netlify.app/assets/Disney.8b0c428a59d97c700130145887eed726.png"} />
-        <Project image={"https://infallible-aryabhata-ff6849.netlify.app/assets/spotify2%20(2).f5691aaced2be37a36f787022ee19b38.png"} />
+        <Project image={"https://arundhaas1.netlify.app/assets/Messenger.26f3f3fdbee968d8e4f546ad07befa47.png"} />
+        <Project image={"https://arundhaas1.netlify.app/assets/Disney.8b0c428a59d97c700130145887eed726.png"} />
+        <Project image={"https://arundhaas1.netlify.app/assets/spotify2%20(2).f5691aaced2be37a36f787022ee19b38.png"} />
         <Project image={"https://vishal2612200.github.io/college-website-design/images/project/priyaart.jpg"} />
         <Project image={"https://vishal2612200.github.io/college-website-design/images/project/todolist.png"} />
       </div>
